00b3fd09d423d5562e4ea9b0ba3bf869e858089d
Yuhao Zhang <380149928@qq.com>
2024-03-23 18:47:54 +0800

    A trick for debuging

---
+++ include/Debug.h
@@ -0,0 +1,12 @@
+#define DEBUG(x) 						\
+	do { 								\
+		std::cerr << x << std::endl; 	\
+	} while(0)
+
+#define ASSERT(x, y) 					\
+	do { 								\
+		if (!(x)) { 					\
+			DEBUG(y); 					\
+			abort(); 					\
+		} 								\
+	} while(0)
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "ParticleSystem.h"
+#include <iostream>
+
+int main() {
+    ParticleSystem ps(1000, 0.01f);
+}
